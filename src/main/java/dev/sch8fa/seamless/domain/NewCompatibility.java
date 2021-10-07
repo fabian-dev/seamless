3b2ad02d37f3bcf2c936ecebad1b75bda60533fe
sch8fa <fschaefer0@gmail.com>
2021-10-07 12:32:08 +0200

    resolve issue after jackson has been updated

--- src/main/java/dev/sch8fa/seamless/domain/NewCompatibility.java
+++ src/main/java/dev/sch8fa/seamless/domain/NewCompatibility.java
@@ -1,13 +1,10 @@
 package dev.sch8fa.seamless.domain;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-// annotation can be removed after spring boot bumps jackson-databind to at least 2.12.0
-@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-public record NewCompatibility(@NotNull String softwareName,
-                               @NotNull String softwareVersion,
-                               @NotNull String componentName,
-                               @NotNull String componentVersion) {
+public record NewCompatibility(@NotNull @NotEmpty String softwareName,
+                               @NotNull @NotEmpty String softwareVersion,
+                               @NotNull @NotEmpty String componentName,
+                               @NotNull @NotEmpty String componentVersion) {
 }
